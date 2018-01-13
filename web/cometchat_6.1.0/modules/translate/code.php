2abe7b3a1d8f7b6f801d2c8fd8dcd12f0fd1b8bb
se <s.epstein05@gmail.com>
2018-01-13 07:15:38 +0000

    removed cometchat_6.1.0

--- web/cometchat_6.1.0/modules/translate/code.php
+++
@@ -1,3 +0,0 @@
-<?php
-$trayiconinfo = array('translate','Page Translate');
-$trayicondata = 'JHRyYXlpY29uW10gPSBhcnJheSgndHJhbnNsYXRlJywnVHJhbnNsYXRlIFRoaXMgUGFnZScsJ21vZHVsZXMvdHJhbnNsYXRlL2luZGV4LnBocCcsJ19wb3B1cCcsJzI4MCcsJzMxMCcsJycsJzEnLCcnKTs=';
